0e9e7d0f3f606c5e5205e444d7bf420b09a839f1
pradakeef <69457383+pradakeef@users.noreply.github.com>
2023-08-13 17:49:37 +1200

    Added styling to Poll and Mean

--- src/components/Poll/Poll.js
+++ src/components/Poll/Poll.js
@@ -5,24 +5,29 @@ const Poll = () => {
   const [cycle, setCycle] = useState(1);
   const [selectedOption, setSelectedOption] = useState(null);
 
+  const resetText = () => {
+    window.location.reload(false);
+
+};
+
   const questions = [
-    'Which picture do you prefer in Cycle 1?',
-    'Which picture do you prefer in Cycle 2?',
-    'Which picture do you prefer in Cycle 3?',
-    'Which picture do you prefer in Cycle 4?',
-    'Which picture do you prefer in Cycle 5?',
+    'Lets start of easy, Man of Steel takes on John Wick?',
+    'OK True, What about Kakarot vs John Wick? Who wins?',
+    'Interesting, What if it was Thanos against John fking Wick',
+    'BRO JOHN FKEN WICK VS ARTHUR FKEN MORGAN CAAARRRTNT?',
+    'The Father of the Atomic Bomb vs John Wick?',
   ];
 
   const images = [
-    ['picture1-cycle1.jpg', 'picture2-cycle1.jpg'],
-    ['picture1-cycle2.jpg', 'picture2-cycle2.jpg'],
-    ['picture1-cycle3.jpg', 'picture2-cycle3.jpg'],
-    ['picture1-cycle4.jpg', 'picture2-cycle4.jpg'],
-    ['picture1-cycle5.jpg', 'picture2-cycle5.jpg'],
+    ['/images/picture1-cycle1.jpg', '/images/picture2-cycle1.jpg'],
+    ['/images/picture1-cycle2.jpg', '/images/picture2-cycle2.jpg'],
+    ['/images/picture1-cycle3.jpg', '/images/picture2-cycle3.jpg'],
+    ['/images/picture1-cycle4.jpg', '/images/picture2-cycle4.jpg'],
+    ['/images/picture1-cycle5.jpg', '/images/picture2-cycle5.jpg'],
   ];
 
   const handleOptionSelect = () => {
-    if (cycle < 5) {
+    if (cycle <= 5) {
       setSelectedOption(null);
       setCycle(cycle + 1);
     }
@@ -30,25 +35,36 @@ const Poll = () => {
 
   return (
     <div className="poll-container">
-      <h1>{questions[cycle - 1]}</h1>
-      <div className="options-container">
-        <div
-          className={`option ${selectedOption === 'option1' ? 'selected' : ''}`}
-          onClick={() => setSelectedOption('option1')}
-        >
-          <img src={images[cycle - 1][0]} alt="Option 1" />
+    
+      {cycle <= 5 ? (
+        <>
+          <h1>{questions[cycle - 1]}</h1>
+          <div className="options-container">
+            <div
+              className={`option ${selectedOption === 'option1' ? 'selected' : ''}`}
+              onClick={() => setSelectedOption('option1')}
+            >
+              <img className="option1photo" src={images[cycle - 1][0]} alt="Option 1" />
+            </div>
+            <div
+              className={`option ${selectedOption === 'option2' ? 'selected' : ''}`}
+              onClick={() => setSelectedOption('option2')}
+            >
+              <img className="option2photo" src={images[cycle - 1][1]} alt="Option 2" />
+            </div>
+          </div>
+          {selectedOption && cycle <= 5 && (
+            <button className="next-button" onClick={handleOptionSelect}>
+              Next
+            </button>
+          )}
+        </>
+      ) : (
+        <div className="congrats-message">
+          <h1>Congrats my guy!</h1>
+          <p>John Wick is the goat.</p>
+          <button className="reset-button" onClick={resetText}>Reset</button>
         </div>
-        <div
-          className={`option ${selectedOption === 'option2' ? 'selected' : ''}`}
-          onClick={() => setSelectedOption('option2')}
-        >
-          <img src={images[cycle - 1][1]} alt="Option 2" />
-        </div>
-      </div>
-      {selectedOption && cycle < 5 && (
-        <button className="next-button" onClick={handleOptionSelect}>
-          Next
-        </button>
       )}
     </div>
   );
